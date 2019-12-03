8ae4b266c2865b9027a8c416ea06a528e83713f1
roman <roman.c6lle@gmail.com>
2019-12-03 23:04:26 +0100

    change get to resources/Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'orders/show'
-  get 'orders/new'
-  get 'cart/show'
-  get 'items/index'
-  get 'items/show'
-  get 'items/new'
-  get 'items/edit'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders
+  resources :items
+  resources :cart
+  root 'items#index'
 end
