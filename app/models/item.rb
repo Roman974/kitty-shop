ccb208f22961324804657b1b09e52edae5b42924
roman <roman.c6lle@gmail.com>
2019-12-03 14:58:04 +0100

    [CREATE] joint table order items/Done

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :join_table_cart_items
   has_many :carts, through: :join_table_cart_items
+  has_many :join_table_order_items
+  has_many :items, through: :join_table_order_items
 end
