4b72998d1246bff02dc3b979d2a7bd8a1dacf65f
roman <roman.c6lle@gmail.com>
2019-12-03 15:45:54 +0100

    [CREATE] bind migrations/Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   
   has_one :cart
   has_many :items #En tant que vendeur
+  has_many :bought_items, class_name: "Item"
   has_many :orders # Là où se trouve les items achetés
 end
