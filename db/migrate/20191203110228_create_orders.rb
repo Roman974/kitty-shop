73a6a0fe3afb6112f69cbcb116a22ff2caa9e22b
roman <roman.c6lle@gmail.com>
2019-12-03 19:57:59 +0100

    [UPDATE] fixed error in order model/Done

--- db/migrate/20191203110228_create_orders.rb
+++ db/migrate/20191203110228_create_orders.rb
@@ -1,8 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.belongs_to :user
-
+      t.belongs_to :cart
       t.timestamps
     end
   end
