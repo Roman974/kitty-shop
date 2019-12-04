af464a22bb0b4cba74bd8d0376369acbfa25662e
roman <roman.c6lle@gmail.com>
2019-12-04 10:29:01 +0100

    [CREATE] back: access only in your page ok /On going

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,12 @@
 class UserController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :orders
   resources :items
   resources :cart
+  resources :user
   root 'items#index'
 end
